c86aea788b54298ce6c04d224c0327f525be27a8
Hyunjin <roykim0823@gmail.com>
2018-10-23 19:27:27 -0700

    10232018

--- Debug/4_FixingMemoryProblems/src/main1.c
+++ Debug/4_FixingMemoryProblems/src/main1.c
@@ -8,11 +8,10 @@ int main(int argc, char *argv[]) {
 	int *A = (int*) malloc(sizeof(int)*size);
 
 	for(n=size; n>0; n--)		// A[100]...A[1], Invalid write of size 4 at A[100]
-	// for(n=size-1; n>=0; n--)	// Corrected
+	//for(n=size-1; n>=0; n--)	// Corrected
 		A[n]=n;                 // Error: A[100] invalid write
 	for(n=0; n<size; n++)       // A[0]...A[99]
 		sum+=A[n];              // Error: A[0] is not initialized
 	printf("sum = %d\n", sum);
-	return 0;
-	// No free(): mem leak A[]
+	return 0;                   // No free(): mem leak: A[]
 }
